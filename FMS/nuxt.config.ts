33631cdd239b4a811b831ebcb7de9f1edb0b5b85
hamidh-2k8 <hamidh.2k8@gmail.com>
2026-07-17 11:10:49 -0400

    Added Tailwind, shadcn-ui, new pages

--- FMS/nuxt.config.ts
+++ FMS/nuxt.config.ts
@@ -1,5 +1,20 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
+import tailwindcss from "@tailwindcss/vite";
+
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+  css: ["~/assets/style.css"],
+  vite: {
+    plugins: [
+      tailwindcss(),
+    ],
+    optimizeDeps: {
+      include: [
+        '@vue/devtools-core',
+        '@vue/devtools-kit',
+      ]
+    }
+  },
+
+  modules: ['@nuxtjs/color-mode']
 })
